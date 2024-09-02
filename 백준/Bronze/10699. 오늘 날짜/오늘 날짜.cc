9e060f41774890e794686fbac29949a588cf0760
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-09-02 23:13:36 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "2024-09-02";
+
+    return 0;
+}
