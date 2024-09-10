67fc17560f5f27297f4ee690d96d03f9c231c8dc
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-09-10 14:01:31 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A, B;
+
+    while (cin >> A >> B)
+    {
+        cout << A + B << "\n";
+    }
+
+    return 0;
+}
