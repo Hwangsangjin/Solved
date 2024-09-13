6cb0b1cd5a001f376d71321f04511540911405f7
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-09-13 15:33:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char Input;
+    cin >> Input;
+    cout << (int)Input;
+
+    return 0;
+}
