9ae4d44f43d941bd902065e553ff583e55f2ab86
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-09-14 10:11:35 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string Input;
+    cin >> Input;
+
+    cout << Input.length();
+
+    return 0;
+}
