00fa8757a949ce63e62973512a7731e93c726f4b
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-09-17 13:31:00 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string Input;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        getline(cin, Input);
+        cout << Input << "\n";
+    }
+
+    return 0;
+}
