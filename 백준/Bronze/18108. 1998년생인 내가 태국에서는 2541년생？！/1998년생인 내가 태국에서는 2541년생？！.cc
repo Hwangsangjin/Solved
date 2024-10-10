cfe86ef54f193f08a0664b79a52865657f5aff63
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2024-10-10 15:34:23 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int Input;
+    cin >> Input;
+    cout << Input - 543;
+
+    return 0;
+}
