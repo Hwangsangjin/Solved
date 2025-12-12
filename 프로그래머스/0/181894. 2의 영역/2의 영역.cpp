5335d497db7642a7ea152555e669f08ffd3da706
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2025-12-12 22:12:33 +0900

    [level 0] Title: 2의 영역, Time: 23.04 ms, Memory: 23.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    vector<int> answer;
+    
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (arr[i] == 2)
+            answer.push_back(i);
+    }
+    
+    if (answer.empty())
+        return {-1};
+    
+    return vector<int>(arr.begin() + answer[0], arr.begin() + answer[answer.size() - 1] + 1);
+}
