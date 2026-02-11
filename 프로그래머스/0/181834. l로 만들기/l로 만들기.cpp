2db1e163eaf4aa9ac9c2d4c8f1a620b0c6a5bf23
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-02-11 17:30:07 +0900

    [level 0] Title: l로 만들기, Time: 0.52 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+string solution(string myString)
+{
+    for (char& c : myString)
+    {
+        if (c < 'l')
+            c = 'l';
+    }
+    
+    return myString;
+}
