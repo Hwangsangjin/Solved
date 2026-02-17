980ba5ad27089a32db1b61ad737a6b25e61cbae7
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-02-17 15:29:20 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -0,0 +1,17 @@
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int numer1, int denom1, int numer2, int denom2)
+{
+    int numer = numer1 * denom2 + numer2 * denom1;
+    int denom = denom1 * denom2;
+    
+    int g = gcd(numer, denom);
+    
+    numer /= g;
+    denom /= g;
+    
+    return {numer, denom};
+}
