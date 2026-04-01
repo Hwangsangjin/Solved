2e82d572eec28a725ac24a9ae3f3c88c3a40d967
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-04-01 22:51:00 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cpp
@@ -0,0 +1,25 @@
+int solution(int n)
+{
+    int answer = 0;
+    
+    for (int i = 4; i <= n; ++i)
+    {
+        int count = 0;
+        
+        for (int j = 1; j * j <= i; ++j)
+        {
+            if (i % j == 0)
+            {
+                if (j * j == i)
+                    count += 1;
+                else
+                    count += 2;
+            }
+        }
+        
+        if (count >= 3)
+            ++answer;
+    }
+    
+    return answer;
+}
