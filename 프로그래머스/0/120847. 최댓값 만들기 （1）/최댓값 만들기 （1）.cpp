100dc2b7dfe64e3071db124bd3646c6ebfa8724b
Hwangsangjin <59340471+Hwangsangjin@users.noreply.github.com>
2026-04-02 22:15:08 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers)
+{
+    sort(numbers.begin(), numbers.end(), greater<>());
+    return numbers[0] * numbers[1];
+}
